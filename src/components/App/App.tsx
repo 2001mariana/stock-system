2c29c3041f34e1bdddbf00aa68ec86c8d9e45380
2001mariana <alves2001mariana@outlook.com>
2022-07-20 14:37:51 -0300

    clean for start app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,9 @@
-import FirstComponent from '../FirstComponent';
-import logo from './logo.svg';
-
-import './App.css';
-import ClassComponent from '../ClassComponent';
-import LifeCycle from '../LifeCycle';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-         <img src={logo} className="App-logo" alt="logo" />
-       
-        <FirstComponent />
-        <ClassComponent example={'first props'} />
-        <LifeCycle />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }

41685586ba7d9f07d7e04e21262ab94c2246eebc
2001mariana <alves2001mariana@outlook.com>
2022-07-21 10:42:11 -0300

    create input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import Button from '../../sharedComponents/Button'
 import Container from '../../sharedComponents/Container'
+import Input from '../../sharedComponents/Input'
 import Header from '../Header/Header'
 
 import './App.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <div className="App">
       <Header title="Header" />
@@ -16,6 +20,12 @@ function App() {
           variant="solid"
           color={'primary'}
           onClick={() => console.log('clicou no botão')}
+        />
+        <Input
+          label="AAA"
+          placeholder="aa"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </Container>
     </div>

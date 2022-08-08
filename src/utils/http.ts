4d010e22e63eaa8875158d18a79081194eaf4223
2001mariana <alves2001mariana@outlook.com>
2022-08-07 21:45:37 -0300

    mock server

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
-const http = axios.create()
+const http = axios.create({
+  baseURL: 'http://127.0.0.1:4010',
+  headers: {
+    authorization: 'Bearer 123'
+  }
+})
 
 export default http

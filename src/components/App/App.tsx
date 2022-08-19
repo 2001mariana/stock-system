6a949a0a9baf83b525295eee1a9cc6b7c6eb22ce
2001mariana <alves2001mariana@outlook.com>
2022-08-19 14:04:00 -0300

    created profile view and card

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import HomeView from '../../views/HomeView/HomeView'
 import LoginView from '../../views/LoginView/LoginView'
 import NotFoundView from '../../views/NotFoundView/NotFoundView'
+import ProfileView from '../../views/ProfileView/ProfileView'
 
 import './App.css'
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeView />} />
           <Route path="/login" element={<LoginView />} />
+          <Route path="/profile" element={<ProfileView />} />
           <Route path="*" element={<NotFoundView />} />
         </Routes>
       </BrowserRouter>
---
+++ src/views/ProfileView/ProfileView.tsx
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux'
+
+import ProfileCard, { User } from '../../components/Authentication/ProfileCard'
+import Header from '../../components/Header'
+import Container from '../../sharedComponents/Container'
+
+declare interface ProfileViewProps {
+  user: User
+}
+
+const ProfileView = ({ user }: ProfileViewProps) => {
+  return (
+    <>
+      <Header title="AlgaStock" />
+      <Container>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <ProfileCard user={user} />
+        </div>
+      </Container>
+    </>
+  )
+}
+
+const mapStateToProps = () => ({
+  user: {
+    name: 'Mariana Luiza',
+    email: 'mariana.luiza@algaworks.com'
+  }
+})
+
+export default connect(mapStateToProps)(ProfileView)

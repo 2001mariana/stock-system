b7795e3964230f7ff033a18dde306dec03824efb
2001mariana <alves2001mariana@outlook.com>
2022-08-23 14:40:18 -0300

    add bearer in interceptor request

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,21 @@
 import axios from 'axios'
+import { store } from '../redux'
 
 const http = axios.create({
-  baseURL: 'http://127.0.0.1:3024',
-  headers: {
-    authorization: 'Bearer 123'
+  baseURL: 'http://127.0.0.1:3024'
+})
+
+http.interceptors.request.use((config) => {
+  const token = store.getState().authentication.profile?.token
+
+  const headers = {
+    ...config.headers,
+    Authorization: `Bearer ${token}`
   }
+
+  token && (config.headers = headers)
+
+  return config
 })
 
 export default http
